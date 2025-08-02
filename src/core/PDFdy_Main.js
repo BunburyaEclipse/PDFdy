7cc64d64d7ccfabea86aef5488e30bc4e9165808
BunburyaEclipse <sirbastidas09@gmail.com>
2025-08-01 20:49:40 -0500

    remove adapter in PDFdy_Main constructor (we dont need it)

--- src/core/PDFdy_Main.js
+++ src/core/PDFdy_Main.js
@@ -1,7 +1,6 @@
 // Esta clase esta inconclusa, es la principal la cual manejara toda la logica
 class PDFdy_Main {
-	constructor(adapter, template, theme, data){
-		this.adapter = adapter;
+	constructor(template, theme, data){
 		this.template = template;
 		this.theme = theme;
 		this.data = data;
